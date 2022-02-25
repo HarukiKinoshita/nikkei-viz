32d0f42b7a4de700389bb660d2bcfc79f4529988
HarukiKinoshita <harukikinoshita@outlook.com>
2022-02-26 03:51:30 +0900

    Add weight on line

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="home columns is-multiline is-gapless">
-    <div class="column is-12-tablet is-9-desktop mb-4">
+    <div class="column is-7-tablet is-8-fullhd mb-4">
       <l-map id="map" :zoom.sync="zoom" :center="center">
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
         <l-control position="topleft" />
 
         <!-- Path  -->
-        <l-polyline v-for="(group, index) in groups" :key="`path-`+index" :lat-lngs="group.latlngs" :opacity="0.5" color="#CC415A" :weight="0.2*zoom" :renderer="myRenderer">
-          <l-tooltip>
-            <strong>{{ groups[index].lastaddress }} ▶︎ {{ groups[index].m_camp }} : {{ groups[index].pop.toLocaleString() }}</strong>:
-          </l-tooltip>
-        </l-polyline>
+        <l-polyline v-for="(group, index) in groups" :key="`path-`+index"  :lat-lngs="group.latlngs" :opacity="0.4" color="crimson" :weight="group.pop/700 + 1" lineCap="square" />
+        <!-- <l-polyline :lat-lngs="group.latlngs" :opacity="0.5" :color="`hsl(${Math.log(group.pop-0.9)*(-15) + 80}, 75%, 50%)`" :weight="0.2*zoom" /> -->
 
         <!-- Last address -->
         <l-circle
@@ -57,41 +54,44 @@
     </div>
     <!-- <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <div class="column is-12-tablet is-3-desktop">
-      <div v-if="targetLocationInfo" class="mb-6">
-        <p class="mb-4 is-family-serif">
-          <span class="has-text-weight-bold is-family-sans-serif">{{ targetLocationInfo.pop.toLocaleString() }}</span> people were
-          <span v-if="targetLocationInfo.type == 'Place of Residence'">residing in</span>
-          <span v-else>relocated to</span>
-        </p>
-        <p class="title is-3">{{ targetLocationName }}</p>
-        <p class="subtitle" v-if="targetLocationInfo.type !== 'Place of Residence'">{{ targetLocationInfo.type }}</p>
-      </div>
+    <div class="column is-5-tablet is-4-fullhd">
+      <div class="px-4">
+        <div v-if="targetLocationInfo" class="mb-6">
+          <p class="mb-4 is-family-serif">
+            <span class="has-text-weight-bold is-family-sans-serif">{{ targetLocationInfo.pop.toLocaleString() }}</span> people were
+            <span v-if="targetLocationInfo.type == 'Place of Residence'">residing in</span>
+            <span v-else>relocated to</span>
+          </p>
+          <p class="title is-3">{{ targetLocationName }}</p>
+          <p class="subtitle" v-if="targetLocationInfo.type !== 'Place of Residence'">{{ targetLocationInfo.type }}</p>
+        </div>
 
-      <div class="my-6" v-if="mode == 'DEBUG'">
-        <table class="table is-bordered" style="margin: auto;">
-          <tr v-for="(value, key) in targetLocationInfo" :key="key">
-            <td>{{ key }}</td>
-            <td>{{ value }}</td>
-          </tr>
-        </table>
-      </div>
+        <div class="my-6" v-if="mode == 'DEBUG'">
+          <table class="table is-bordered" style="margin: auto;">
+            <tr v-for="(value, key) in targetLocationInfo" :key="key">
+              <td>{{ key }}</td>
+              <td>{{ value }}</td>
+            </tr>
+          </table>
+        </div>
 
-      <div v-if="targetLocationCategory == 'm_camp'">
-        <p class="is-family-serif">They came from ...</p>
-        <table class="table is-narrow my-4" style="margin: auto;">
-          <tr v-for="(item, index) in groups" :key="index" class="has-text-right">
-            <td>{{ item.lastaddress }}, {{ item.m_originalstate }}</td><td>{{ item.pop.toLocaleString() }}</td>
-          </tr>
-        </table>
-      </div>
-      <div v-if="targetLocationCategory == 'lastaddress'">
-        <p class="is-family-serif">They were sent to ...</p>
-        <table class="table is-narrow my-4" style="margin: auto;">
-          <tr v-for="(item, index) in groups" :key="index" class="has-text-right">
-            <td>{{ item.m_camp }}</td><td>{{ item.pop.toLocaleString() }}</td>
-          </tr>
-        </table>
+        <div v-if="targetLocationCategory == 'm_camp'">
+          <p class="is-family-serif">They came from ...</p>
+          <table class="table is-narrow my-4" style="margin: auto;">
+            <tr v-for="(item, index) in groups" :key="index" class="has-text-right">
+              <td>{{ item.lastaddress }}, {{ item.m_originalstate }}</td><td>{{ item.pop.toLocaleString() }}</td>
+              <!-- <td><p :style="`background-color: hsl(${Math.log(item.pop-0.9)*(-15) + 80}, 75%, 50%)`">{{ Math.log(item.pop-0.9)*(-15) + 70 }}</p></td> -->
+            </tr>
+          </table>
+        </div>
+        <div v-if="targetLocationCategory == 'lastaddress'">
+          <p class="is-family-serif">They were sent to ...</p>
+          <table class="table is-narrow my-4" style="margin: auto;">
+            <tr v-for="(item, index) in groups" :key="index" class="has-text-right">
+              <td>{{ item.m_camp }}</td><td>{{ item.pop.toLocaleString() }}</td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -108,7 +108,7 @@ import wra_master_assemblycenter from '../datasets/wra-master-assemblycenter.jso
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
+    HelloWorld
   },
   data () {
     return {
@@ -147,23 +147,25 @@ export default {
       this.targetLocationName = this[cat][index][cat]
       this.targetLocationInfo = this[cat][index]
       // this.$set(this.$refs[cat][index], 'fillOpacity', 0.8)
-      // console.log(this[cat][index][cat]);
-      console.log(this.$refs[cat][index].name)
+      console.log(this[cat][index][cat]);
+      // console.log(this.$refs[cat][index].name)
     },
     removeTarget() {
       this.targetLocationCategory = ""
       this.targetLocationName = ""
       this.targetLocationInfo = ""
     },
-    // getLineColor(index) {
-    //   return this.colorScale4[(index/400).toFixed()]
-    // },
     getLineWeight(index) {
       return (index/400).toFixed()*0.4*this.zoom
+    },
+    getLineColor(pop) {
+      return `hsl(${(pop / 7000) * 90}, 75%, 50%)`;
     }
   },
   mounted() {
     console.log(process.env.VUE_APP_DEV_MODE);
+  },
+  updated() {
   },
   computed: {
     groups() {
@@ -213,8 +215,10 @@ export default {
   #map {
     height: 75vh;
   }
-  #map_lastaddress {
-    height: 75vh;
+  @media screen and (max-width: 768px) {
+    #map {
+      height: 60vh;
+    }
   }
   .is-family-serif {
     font-family: Georgia, Times, 'Times New Roman', serif
